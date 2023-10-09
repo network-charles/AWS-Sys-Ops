a0fc06b439e9f3b04a8c6fbd0c969e32a77ee77d
Charles Uneze <charlesniklaus@gmail.com>
2023-10-09 12:43:03 +0100

    Update output.tf

--- Lab 2/output.tf
+++ Lab 2/output.tf
@@ -1,8 +1,5 @@
 # aws ssm start-session --target i-09711f32a214339dd
 
-# aws ec2-instance-connect ssh --instance-id i-09711f32a214339dd
-
-
 output "EC2_ID" {
   value = aws_instance.Instance.id
 }
