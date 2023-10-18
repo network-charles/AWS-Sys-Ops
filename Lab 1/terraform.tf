f0e4c8930ca5da9d76d3e39e3aa6c9b0f9e907b0
Charles Uneze <charlesniklaus@gmail.com>
2023-10-18 12:54:56 +0100

    Update terraform.tf

--- Lab 1/terraform.tf
+++ Lab 1/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = ""
     key            = "terraform.tfstate"
-    region         = "us-west-2"
+    region         = "eu-west-2"
     dynamodb_table = ""
   }
 
@@ -16,7 +16,7 @@ terraform {
 
 # Configure the AWS Provider for Account A
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-2"
 
   default_tags {
     tags = {
