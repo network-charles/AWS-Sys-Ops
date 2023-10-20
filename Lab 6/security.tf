2e32c03cf0374f8b5fbe5477a819e93979a9d810
Charles Uneze <charlesniklaus@gmail.com>
2023-10-20 13:34:19 +0100

    Create security.tf

---
+++ Lab 6/security.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "SG" {
+  vpc_id      = aws_vpc.VPC.id
+  description = "SG"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "SG"
+  }
+}
+
+resource "aws_kms_key" "mysql_aurora" {
+  description             = "mysql_aurora"
+  deletion_window_in_days = 7
+
+  policy = data.aws_iam_policy_document.kms.json
+
+  tags = {
+    "Name" = "mysql_aurora"
+  }
+}
+
+resource "aws_iam_role" "aurora_kms_role" {
+  name = "AuroraKMSRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "rds.amazonaws.com"
+        }
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "kms.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
