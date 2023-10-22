356575479632b03afeb3be0805938ffc52f968da
Charles Uneze <charlesniklaus@gmail.com>
2023-10-22 14:42:35 +0100

    Create variable.tf

---
+++ Lab 8/variable.tf
@@ -0,0 +1,19 @@
+variable "account_id" {
+  type    = string
+  default = ""
+}
+
+variable "FederatedUserARN" {
+  type    = string
+  default = ""
+}
+
+variable "username" {
+  type = string
+  default = "foo"
+}
+
+variable "password" {
+  type = string
+  default = "Mustbe8characters"
+}
