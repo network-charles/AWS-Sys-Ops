b519f543a956441e00e1ac77bd74f4cf08f95aa7
Charles Uneze <charlesniklaus@gmail.com>
2023-10-22 07:12:14 +0100

    Create variable.tf

---
+++ Lab 7/variable.tf
@@ -0,0 +1,29 @@
+variable "account_id" {
+  type    = string
+  default = ""
+}
+
+variable "FederatedUserARN" {
+  type    = string
+  default = ""
+}
+
+variable "ubuntu" {
+  type    = string
+  default = "ami-0505148b3591e4c07"
+}
+
+variable "key_name" {
+  type    = string
+  default = ""
+}
+
+variable "username" {
+  type = string
+  default = "foo"
+}
+
+variable "password" {
+  type = string
+  default = "foobarbaz"
+}
