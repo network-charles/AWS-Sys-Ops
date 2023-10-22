6d4d5db8d09bbed9c4b614d627662b2f7b406885
Charles Uneze <charlesniklaus@gmail.com>
2023-10-22 07:10:29 +0100

    Create output.tf

---
+++ Lab 7/output.tf
@@ -0,0 +1,12 @@
+output "RDS_Oracle" {
+  value = "sqlplus ${var.username}/${var.password}@${aws_db_instance.Oracle.endpoint}/oracle"
+}
+
+output "Bastion1" {
+  value = "ssh -i \"${var.key_name}.pem\" ubuntu@${aws_instance.Bastion1.public_ip}"
+}
+
+output "Bastion2" {
+  value = "ssh -i \"${var.key_name}.pem\" ubuntu@${aws_instance.Bastion2.public_ip}"
+}
+
