ebb8d02d8a750cc1905a91b23bcc5f0b8c3a9f95
Charles Uneze <charlesniklaus@gmail.com>
2023-10-25 11:54:26 +0100

    Update output.tf

--- Lab 6/output.tf
+++ Lab 6/output.tf
@@ -1,27 +1,27 @@
 output "Aurora_Primary_DB" {
-  value = aws_rds_cluster.MySQL.endpoint
+  value = "mysql -h ${aws_rds_cluster.MySQL.endpoint} -u test -p"
 }
 
 output "Aurora_Writer" {
-  value = aws_rds_cluster_instance.Writer.endpoint
+  value = "mysql -h ${aws_rds_cluster.Writer.endpoint} -u test -p"
 }
 
 output "Aurora_Replica1" {
-  value = aws_rds_cluster_instance.Replica1.endpoint
+  value = "mysql -h ${aws_rds_cluster.Replica1.endpoint} -u test -p"
 }
 
 output "Aurora_Replica2" {
-  value = aws_rds_cluster_instance.Replica2.endpoint
+  value = "mysql -h ${aws_rds_cluster.Replica2.endpoint} -u test -p"
 }
 
 output "Bastion1" {
-  value = aws_instance.Bastion1.public_ip
+  value = "ssh -i '${var.key_name}.pem' ubuntu@${aws_instance.Bastion1.public_ip}"
 }
 
 output "Bastion2" {
-  value = aws_instance.Bastion2.public_ip
+  value = "ssh -i '${var.key_name}.pem' ubuntu@${aws_instance.Bastion2.public_ip}"
 }
 
 output "Bastion3" {
-  value = aws_instance.Bastion3.public_ip
+  value = "ssh -i '${var.key_name}.pem' ubuntu@${aws_instance.Bastion3.public_ip}"
 }
