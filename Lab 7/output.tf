e2052c14cfd47d6e36220faeab0c2630bf94273b
Charles Uneze <charlesniklaus@gmail.com>
2023-10-25 10:21:27 +0100

    Update output.tf

--- Lab 7/output.tf
+++ Lab 7/output.tf
@@ -3,10 +3,10 @@ output "RDS_Oracle" {
 }
 
 output "Bastion1" {
-  value = "ssh -i \'${var.key_name}.pem\' ubuntu@${aws_instance.Bastion1.public_ip}"
+  value = "ssh -i '${var.key_name}.pem' ubuntu@${aws_instance.Bastion1.public_ip}"
 }
 
 output "Bastion2" {
-  value = "ssh -i \'${var.key_name}.pem\' ubuntu@${aws_instance.Bastion2.public_ip}"
+  value = "ssh -i '${var.key_name}.pem' ubuntu@${aws_instance.Bastion2.public_ip}"
 }
 
