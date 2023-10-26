b8f6dc2910a03bf499d74e7d56aa04e931a4e178
Charles Uneze <charlesniklaus@gmail.com>
2023-10-26 23:12:50 +0100

    Update output.tf

--- Lab 10/output.tf
+++ Lab 10/output.tf
@@ -2,10 +2,6 @@ output "Redis_Endpoint" {
   value = "redis-cli -h ${aws_elasticache_replication_group.Primary.configuration_endpoint_address} -p 6379"
 }
 
-output "Bastion1" {
-  value = "ssh -i '${var.key_name}.pem' ubuntu@${aws_instance.Bastion1.public_ip}"
-}
-
 /*
 Key-Value Store:
 SET username:1 "john_doe"
