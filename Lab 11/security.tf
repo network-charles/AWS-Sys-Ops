885a694226cf85048e67f58e3052a78b89a95881
Charles Uneze <charlesniklaus@gmail.com>
2023-10-27 09:43:26 +0100

    Create security.tf

---
+++ Lab 11/security.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "SG" {
+  vpc_id      = aws_vpc.VPC.id
+  description = "SG"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 11211 # Memcached default port
+    to_port     = 11211
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "SG"
+  }
+}
